e02d1235b66eb94daa967686f9474dcee320db94
Teodor <te0gop.u@gmail.com>
2021-11-05 20:40:29 +0200

    corona virus tracker

--- Coronavirus-Tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
+++ Coronavirus-Tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -19,19 +19,17 @@ public class HomeController {
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalReportedCases = allStats.stream()
-                .mapToInt(stat -> stat.getLatestTotalCases())
+                .mapToInt(LocationStats::getLatestTotalCases)
                 .sum();
 
         int totalNewCases = allStats.stream()
-                .mapToInt(stat -> stat.getDiffFromPrevDay())
+                .mapToInt(LocationStats::getDiffFromPrevDay)
                 .sum();
 
 
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
-
-
         return "home";
     }
 }
