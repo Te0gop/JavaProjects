ee81b0ca2ea632984aa0ff91c29e90df96d919ce
Teodor <te0gop.u@gmail.com>
2021-11-06 16:37:26 +0200

    Spring project with CRUD MySQL, JPA, Hibernate,Thymeleaf and Bootstrap

--- Web_App/src/main/java/io/spring/webapp/MainController.java
+++ Web_App/src/main/java/io/spring/webapp/MainController.java
@@ -1,4 +1,13 @@
 package io.spring.webapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("")
+    public String showHomePage() {
+        return "index";
+    }
 }
