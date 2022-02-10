8135ae8b8d78c2b2a5cac2eb6558b108cbed61d4
Teodor <te0gop.u@gmail.com>
2022-02-10 15:57:40 +0200

    spring boot project using dao layer

---
+++ dao-project/src/main/java/com/springboot/daoproject/DaoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.daoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoProjectApplication.class, args);
+	}
+
+}
---
+++ dao-project/src/test/java/com/springboot/daoproject/DaoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.daoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
