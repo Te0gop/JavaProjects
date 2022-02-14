88ea998ddd9d23a47191d47c24f32761e687c7ea
Teodor <te0gop.u@gmail.com>
2022-02-14 17:45:16 +0200

    students management system

---
+++ students-rest-api/src/main/java/com/spring/restapi/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.spring.restapi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+
+
+
+}
---
+++ students-rest-api/src/main/java/com/spring/restapi/entity/Student.java
@@ -0,0 +1,82 @@
+package com.spring.restapi.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Table(name = "students")
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+
+    @OneToMany
+    @JoinTable(name = "lectures")
+    private List<Lecture> lectures;
+
+    public Student(Long id, String firstName, String lastName, int age, String email, List<Lecture> lectures) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.lectures = lectures;
+    }
+
+    public Student() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Lecture> getLectures() {
+        return lectures;
+    }
+
+    public void setLectures(List<Lecture> lectures) {
+        this.lectures = lectures;
+    }
+}
