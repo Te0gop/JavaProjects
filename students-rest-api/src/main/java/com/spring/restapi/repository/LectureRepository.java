b9e4095bd59da62a2c57c8ea98dac6aa06315431
Te0gop <58914514+Te0gop@users.noreply.github.com>
2022-02-22 21:50:04 +0200

    Delete students-rest-api directory

--- students-rest-api/src/main/java/com/spring/restapi/repository/LectureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.restapi.repository;
-
-import com.spring.restapi.entity.Lecture;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-}
--- students-rest-api/src/main/java/com/spring/restapi/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.restapi.repository;
-
-import com.spring.restapi.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-}
