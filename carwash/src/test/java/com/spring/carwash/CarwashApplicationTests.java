0ffdb3a8e0aa1536607b083957cffb7d21e1f9e0
Teodor <te0gop.u@gmail.com>
2022-05-24 15:04:11 +0300

    setting up the spring project

---
+++ carwash/src/test/java/com/spring/carwash/CarwashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.carwash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarwashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
