62fd98f25a0b562a1b934d02568b9af49209cfa6
Teodor <te0gop.u@gmail.com>
2022-05-29 08:28:44 +0300

    autowiring repo and modifing get methods

--- carwash/src/main/java/com/spring/carwash/web/CarWasherController.java
+++ carwash/src/main/java/com/spring/carwash/web/CarWasherController.java
@@ -1,7 +1,9 @@
 package com.spring.carwash.web;
 
+import com.spring.carwash.dao.CarWasherRepository;
 import com.spring.carwash.model.Car;
 import com.spring.carwash.model.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,48 +18,61 @@ import java.util.List;
 @RequestMapping(value = "/carWasherController")
 public class CarWasherController {
 
-    @RequestMapping("/cars")
-    @ResponseBody
-    public List<Car> getCars() {
-        List<Car> cars = new ArrayList<>();
+//    @RequestMapping("/cars")
+//    @ResponseBody
+//    public List<Car> getCars() {
+//        List<Car> cars = new ArrayList<>();
+//
+//        Driver driver1 = new Driver(1, "Walter", "White", "+359343454632", "St.George str.");
+//        Driver driver2 = new Driver(2, "John", "Smith", "+359343454442", "St.Patrick str.");
+//        Driver driver3 = new Driver(3, "John", "Carter", "+359343452221", "St.Lucas str.");
+//        Car car1 = new Car(1, "FIAT", "Red", driver1, 2018);
+//        Car car2 = new Car(2, "Opel", "Blue", driver2, 2019);
+//        Car car3 = new Car(3, "Citroen", "Gray", driver3, 2020);
+//
+//        cars.add(car1);
+//        cars.add(car2);
+//        cars.add(car3);
+//
+//        return cars;
+//    }
+//
+//    @RequestMapping("/car/{id}")
+//    public ResponseEntity<Car> getCar(@PathVariable("id") int id) {
+//        List<Car> cars = new ArrayList<>();
+//
+//        Driver driver1 = new Driver(1, "Peter", "McCane", "+35934344442", "St.George str.");
+//        Driver driver2 = new Driver(2, "Steve", "Rogers", "+359343354442", "St.Patrick str.");
+//        Driver driver3 = new Driver(3, "Luke", "Green", "+3593434522111", "St.Lucas str.");
+//        Car car1 = new Car(1, "FIAT", "Red", driver1, 2018);
+//        Car car2 = new Car(2, "Opel", "Blue", driver2, 2019);
+//        Car car3 = new Car(3, "Citroen", "Gray", driver3, 2020);
+//
+//        cars.add(car1);
+//        cars.add(car2);
+//        cars.add(car3);
+//
+//        return ResponseEntity.ok(cars.get(id));
+//    }
+//
+//    @RequestMapping("/cache")
+//    public ModelAndView cacheBustingTest() {
+//        ModelAndView modelAndView = new ModelAndView();
+//
+//        modelAndView.setViewName("Cash busting test");
+//        return modelAndView;
+//    }
 
-        Driver driver1 = new Driver(1, "Walter", "White", "+359343454632", "St.George str.");
-        Driver driver2 = new Driver(2, "John", "Smith", "+359343454442", "St.Patrick str.");
-        Driver driver3 = new Driver(3, "John", "Carter", "+359343452221", "St.Lucas str.");
-        Car car1 = new Car(1, "FIAT", "Red", driver1, 2018);
-        Car car2 = new Car(2, "Opel", "Blue", driver2, 2019);
-        Car car3 = new Car(3, "Citroen", "Gray", driver3, 2020);
-
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-
-        return cars;
-    }
+    @Autowired
+    private CarWasherRepository repo;
 
     @RequestMapping("/car/{id}")
-    public ResponseEntity<Car> getCar(@PathVariable("id") int id) {
-        List<Car> cars = new ArrayList<>();
-
-        Driver driver1 = new Driver(1, "Peter", "McCane", "+35934344442", "St.George str.");
-        Driver driver2 = new Driver(2, "Steve", "Rogers", "+359343354442", "St.Patrick str.");
-        Driver driver3 = new Driver(3, "Luke", "Green", "+3593434522111", "St.Lucas str.");
-        Car car1 = new Car(1, "FIAT", "Red", driver1, 2018);
-        Car car2 = new Car(2, "Opel", "Blue", driver2, 2019);
-        Car car3 = new Car(3, "Citroen", "Gray", driver3, 2020);
-
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-
-        return ResponseEntity.ok(cars.get(id));
+    public Car getCarById(@PathVariable("id") int id) {
+        return repo.getCarById(id);
     }
 
-    @RequestMapping("/cache")
-    public ModelAndView cacheBustingTest() {
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("Cash busting test");
-        return modelAndView;
+    @RequestMapping("/cars")
+    public List<Car> getCars() {
+        return repo.getCars();
     }
 }
