eba7eb16bc771eeefcedf4587b925fb4cb67a867
Teodor <te0gop.u@gmail.com>
2022-05-29 11:11:38 +0300

    adding transactional annotations

--- carwash/src/main/java/com/spring/carwash/dao/CarWasherRepository.java
+++ carwash/src/main/java/com/spring/carwash/dao/CarWasherRepository.java
@@ -5,13 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-//@Transactional(rollbackFor = Exception.class)
+@Transactional(rollbackFor = Exception.class)
 public class CarWasherRepository {
 
 //    @Autowired
@@ -32,10 +33,12 @@ public class CarWasherRepository {
     @Autowired
     private EntityManager entityManager;
 
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public List<Car> getCars() {
         return entityManager.createQuery("from Car", Car.class).getResultList();
     }
 
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public Car getCarById(int id) {
         return entityManager.find(Car.class, id);
     }
