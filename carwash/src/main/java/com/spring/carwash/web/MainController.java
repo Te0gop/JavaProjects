6947e7daead4abc660526a8afa3a97ba408541f4
Teodor <te0gop.u@gmail.com>
2022-05-29 15:58:25 +0300

    adding main controller for model and view

---
+++ carwash/src/main/java/com/spring/carwash/web/MainController.java
@@ -0,0 +1,17 @@
+package com.spring.carwash.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/", "/index.html"})
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
