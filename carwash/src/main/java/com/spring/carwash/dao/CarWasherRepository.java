fedb617409580068d0c03e0848ae29551630e1f0
Teodor <te0gop.u@gmail.com>
2022-05-29 08:27:56 +0300

    adding entity manager to repo

--- carwash/src/main/java/com/spring/carwash/dao/CarWasherRepository.java
+++ carwash/src/main/java/com/spring/carwash/dao/CarWasherRepository.java
@@ -7,26 +7,36 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-@Transactional(rollbackFor = Exception.class)
+//@Transactional(rollbackFor = Exception.class)
 public class CarWasherRepository {
 
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+
+//    public List<Car> getCars() {
+//        return jdbcTemplate.query("select * from car where id=1", (rs, rowNum) -> {
+//            Car c = new Car();
+//
+//            c.setId(rs.getInt(1));
+//            c.setColor((rs.getString(1)));
+//            c.setModel((rs.getString(2)));
+//            c.setYear((rs.getInt(3)));
+//            return c;
+//        });
+//    }
+
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private EntityManager entityManager;
 
     public List<Car> getCars() {
-        return jdbcTemplate.query("select * from car where id=1", (rs, rowNum) -> {
-            Car c = new Car();
+        return entityManager.createQuery("from Car", Car.class).getResultList();
+    }
 
-            c.setId(rs.getInt(1));
-            c.setColor((rs.getString(1)));
-            c.setModel((rs.getString(2)));
-            c.setYear((rs.getInt(3)));
-            return c;
-        });
+    public Car getCarById(int id) {
+        return entityManager.find(Car.class, id);
     }
 }
