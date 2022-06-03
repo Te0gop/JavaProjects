d29560759ceaa62a4ef95dbddcfa0f209930fdf6
Teodor <te0gop.u@gmail.com>
2022-06-03 20:22:08 +0300

    autowiring userdetailsservice instance

--- carwash/src/main/java/com/spring/carwash/configuration/SecurityConfiguration.java
+++ carwash/src/main/java/com/spring/carwash/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.spring.carwash.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,24 +16,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .rememberMe()
+                .userDetailsService(userDetailsService);
+
+        http
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .failureForwardUrl("/login.html?loginFailed=true"));
+
         return http.build();
-
-
-//        http
-//                .formLogin(form -> form
-//                        .loginPage("/login")
-//                        .loginProcessingUrl("/login")
-//                        .failureForwardUrl("/login.html?loginFailed=true"));
-//
-//        return http.build();
     }
+    
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
