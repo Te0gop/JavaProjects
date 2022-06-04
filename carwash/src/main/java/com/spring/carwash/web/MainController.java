5e8a5ca94fb4f3f4ccfbe8b87c89b982a32b16e4
Teodor <te0gop.u@gmail.com>
2022-06-04 20:52:44 +0300

    using java mail sender and fake smtp to send email

--- carwash/src/main/java/com/spring/carwash/web/MainController.java
+++ carwash/src/main/java/com/spring/carwash/web/MainController.java
@@ -1,5 +1,8 @@
 package com.spring.carwash.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,11 +10,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @RequestMapping(value = {"/", "/index.html"})
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.setViewName("index");
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo("xxx@gmail.com");
+        simpleMailMessage.setCc("yyy@gmail.com");
+        simpleMailMessage.setText("Index is accessed.");
+
+        javaMailSender.send(simpleMailMessage);
+
         return modelAndView;
     }
 
